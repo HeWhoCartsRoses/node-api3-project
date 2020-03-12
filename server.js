94ec768faf0332af6a98906a6dc2b63cf12f2e5c
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-03-12 13:47:27 -0600

    beggining

--- server.js
+++ server.js
@@ -2,12 +2,32 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.method, req.url, Date.now())
+};
+
+function validateUserId(id) {
+  return function (req, res, next) {
+
+  }
+};
+
+function validateUser(user) {
+  return function (req, res, next) {
+
+  }
+};
+
+function validatePost(post) {
+  return function (req, res, next) {
+
+  }
+};
 
 module.exports = server;
