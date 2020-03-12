4eb18a48798ae7f1c0de7da3b646bbeedf3cc81e
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-03-12 14:10:08 -0600

    finished logger. continuing to read the readme.

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
-
-server.get('/', logger, (req, res) => {
+server.use(express.json());
+server.get("/", logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
 function logger(req, res, next) {
-  console.log(req.method, req.url, Date.now())
-};
+  res.send(`${Date.now()}, ${req.method}, ${req.originalUrl}`);
+  next();
+}
 
 function validateUserId(id) {
-  return function (req, res, next) {
-
-  }
-};
+  return function (req, res, next) {};
+}
 
 function validateUser(user) {
-  return function (req, res, next) {
-
-  }
-};
+  return function (req, res, next) {};
+}
 
 function validatePost(post) {
-  return function (req, res, next) {
-
-  }
-};
+  return function (req, res, next) {};
+}
 
 module.exports = server;
